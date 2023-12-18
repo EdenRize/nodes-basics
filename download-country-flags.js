09d947e86376e17685f73175c2ccd7a72a598fd8
EdenRize <edenrize@gmail.com>
2023-12-18 09:00:16 +0200

    fix download flags

--- download-country-flags.js
+++ download-country-flags.js
@@ -1,19 +1,18 @@
-import jsonData from './newCountries.json' assert { type: 'json' }
 import { utilService } from './services/util.service.js'
 
 downloadCountryFlags()
 function downloadCountryFlags() {
   const countries = getCountries()
 
-  downloadFlags(countries)
   downloadFlags(countries).then(() => {
     console.log('Your flags are ready')
   })
 }
 
 function getCountries() {
-  jsonData.sort((a, b) => b.population - a.population)
-  return jsonData.slice(0, 5)
+  const countries = utilService.readJsonFile('./newCountries.json')
+  countries.sort((a, b) => b.population - a.population)
+  return countries.slice(0, 5)
 }
 
 function downloadFlags(countries) {
